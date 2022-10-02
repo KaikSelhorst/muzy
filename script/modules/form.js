7c40143ee86059ed830068af5c8d451d2a5eeccb
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-10-02 08:36:26 -0400

    feat: Indicator if the email is correct

---
+++ script/modules/form.js
@@ -0,0 +1,29 @@
+export default class FormCheck {
+  constructor(email, correct, incorrect) {
+    this.email = document.querySelector(email);
+    this.correct = correct;
+    this.incorrect = incorrect;
+
+    this.checkEmail = this.checkEmail.bind(this);
+  }
+
+  checkEmail({ target }) {
+    if (target.value.match(/[\w.-]+@[\w-]+\.[\w-.]+/gi)) {
+      target.classList.remove(this.incorrect);
+      target.classList.add(this.correct);
+    } else {
+      target.classList.remove(this.correct);
+      target.classList.add(this.incorrect);
+    }
+  }
+
+  addEventEmail() {
+    this.email.addEventListener("change", this.checkEmail);
+  }
+
+  init() {
+    if (this.email) {
+      this.addEventEmail();
+    }
+  }
+}
