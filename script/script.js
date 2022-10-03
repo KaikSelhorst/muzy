8bbbb7c8aa30ecf409ee47337c064756e7246686
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-10-03 11:11:50 -0400

    Feat: I added animation to the scroll

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ import FormCheck from "./modules/form.js";
 import Horario from "./modules/horario.js";
 import MenuMobile from "./modules/menuMobile.js";
 import Accordion from "./modules/accordion.js";
+import ScrollAnima from "./modules/anima-scroll.js";
 
 const menuMobile = new MenuMobile(
   "[data-menu='button'] ",
@@ -18,3 +19,6 @@ horario.init();
 
 const accordion = new Accordion('[data-accordion="list"] dt', "active");
 accordion.init();
+
+const scrollAnima = new ScrollAnima("[data-anima]", "ativo");
+scrollAnima.init();
