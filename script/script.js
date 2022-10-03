ff8959150b6900127cb93d2f8e0398ba8b12f7a1
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-10-02 21:06:42 -0400

    Feat: I added the feature of accordion in the sections Treatment and faq

--- script/script.js
+++ script/script.js
@@ -1,19 +1,20 @@
 import FormCheck from "./modules/form.js";
 import Horario from "./modules/horario.js";
 import MenuMobile from "./modules/menuMobile.js";
+import Accordion from "./modules/accordion.js";
 
 const menuMobile = new MenuMobile(
   "[data-menu='button'] ",
   "[data-menu='list']",
   "active"
 );
-
 menuMobile.init();
 
 const formulario = new FormCheck("[data-form='email']", "correto", "incorreto");
-
 formulario.init();
 
 const horario = new Horario(".contato-horario", "aberto");
-
 horario.init();
+
+const accordion = new Accordion('[data-accordion="list"] dt', "active");
+accordion.init();
